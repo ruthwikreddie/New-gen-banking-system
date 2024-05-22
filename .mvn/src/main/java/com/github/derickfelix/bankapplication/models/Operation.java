baf71ad1ce81e17e9074f998e71fef015b13c769
ruthwikreddie <125160422+ruthwikreddie@users.noreply.github.com>
2024-05-22 21:50:20 +0530

    Create Operation.java

---
+++ .mvn/src/main/java/com/github/derickfelix/bankapplication/models/Operation.java
@@ -0,0 +1,59 @@
+package com.github.derickfelix.bankapplication.models;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author derickfelix
+ */
+public class Operation {
+
+    private LocalDateTime createdAt;
+    private String accountNumber;
+    private double amount;
+    private Type type;
+
+    public LocalDateTime getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt)
+    {
+        this.createdAt = createdAt;
+    }
+
+    public String getAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber)
+    {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(double amount)
+    {
+        this.amount = amount;
+    }
+
+    public Type getType()
+    {
+        return type;
+    }
+
+    public void setType(Type type)
+    {
+        this.type = type;
+    }
+          
+    public static enum Type {
+        WITHDRAW, DEPOSIT;
+    }
+}
